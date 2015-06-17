0e841bdf4dcaac53dd188b0dc889893ac6a3b282
Paul Codding <pcodding@gmail.com>
2015-06-17 11:33:00 -0500

    Added TSV Import Utility

---
+++ tsv_import.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+import csv
+import json
+
+tsvProjectFile = open(sys.argv[1], 'r')
+projectFile = open('src/projects/repos.json', 'w')
+
+fieldnames = ("projectTitle","projectDescription","featured","category","subcategory", "url", "repoAccount", "repoName", "buttonText", "included")
+reader = csv.DictReader( tsvProjectFile, fieldnames, delimiter='\t')
+reader.next()
+projectFile.write("[")
+items = []
+for row in reader:
+    if row["included"] == "TRUE":
+        category = "{\"categories\": [{\"name\": \""+row["category"]+"\"}," + "{\"name\": \""+row["subcategory"]+"\"}"
+        if row["featured"] == "TRUE": 
+          category += ",{\"name\": \"featured\"}"
+        category += "]"
+        items.append(category + ", \"title\": \"" + row["projectTitle"] + "\", \"projectDescription\": \"" + row["projectDescription"] + "\", \"cta_1_url\": \"" + row["url"] + "\", \"cta_1_text\": \"" + row["buttonText"] + "\", \"repo_account\": \"" + row["repoAccount"] + "\", \"repo_name\": \"" + row["repoName"] + "\"}\n")
+projectFile.write(",".join(items))
+projectFile.write("]")
+projectFile.close()
